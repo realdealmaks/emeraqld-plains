1057ac2e08524554fae3d1e322768111df2d8e27
Shmubster <adrijan.rosjelu@gmail.com>
2025-10-02 20:07:13 +0200

    Add files via upload

---
+++ connector.py
@@ -0,0 +1,61 @@
+import json
+import os
+
+# connector
+# this file is for storing data
+
+def connector(main_globals):
+    default_data = {
+        "settings": {
+            "music": 100,
+            "sfx": 100,
+            "resolution": (1280, 720),
+            "framerate": 60
+        }
+    }
+
+    class Connector:
+        def __init__(self, filename="data.json", default_data=None):
+            self.filename = filename
+            self.default_data = default_data or {}
+            self.data = {}
+            self.load_data()
+
+        def get_data(self):
+            return self.data
+
+        def set_data(self, new_data):
+            self.data = new_data
+
+        def save_data(self):
+            with open(self.filename, "w") as f:
+                json.dump(self.data, f, indent=4)
+            return self.data
+
+        def load_data(self):
+            if os.path.exists(self.filename):
+                try:
+                    with open(self.filename, "r") as f:
+                        self.data = json.load(f)
+                    # if file is empty
+                    if not self.data:
+                        self.data = self.default_data
+                        self.save_data()
+                except json.JSONDecodeError:
+                    # if file is corrupt and goes against israel
+                    self.data = self.default_data
+                    self.save_data()
+            else:
+                # if file doesnt exist
+                self.data = self.default_data
+                self.save_data()
+            return self.data
+
+    main_globals['connector_instance'] = Connector(default_data=default_data)
+
+# test to see if the file gets written anything
+test = False
+if test == True:
+    main_globals = {}
+    connector(main_globals)
+    print(main_globals['connector_instance'].get_data())
